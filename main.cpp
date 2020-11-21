5bfeb1c37acaec19223ed038af3a8c145e6bc7ee
Felix Hädicke <felixhaedicke@web.de>
2020-11-21 22:17:06 +0100

    New setting "customStyle" to explicitly specify style in application settings

--- main.cpp
+++ main.cpp
@@ -22,6 +22,16 @@ int main(int argc, char *argv[])
         settings.setValue("customIconTheme", QString());
     }
 
+    QString customStyle = settings.value("customStyle").toString();
+    if (!customStyle.isNull() && !customStyle.isEmpty())
+    {
+        QApplication::setStyle(customStyle);
+    }
+    else
+    {
+        settings.setValue("customStyle", QString());
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
